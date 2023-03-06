f9b4ef59e85ad7a5b3c2f4f944fd4bed5527bde3
wangpeng20 <wangpeng20@baidu.com>
2023-03-06 12:59:53 +0800

    fix

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def ask_openai(question, user_id):
     if context==None:
         context={
                 "messages":[
-                    {"role": "system", "content": "You are a helpful assistant."},
                     ]
         }
     context["messages"].append(
@@ -68,28 +67,52 @@ def ask_openai(question, user_id):
 
 
 
-def seed():
-    print("hello world")
-    return gr.update(default=random.randint(1, 100000000000))
 
 
-# 创建一个UI界面，并指定输入和输出的类型
-question_box = gr.inputs.Textbox(label="Question") # 创建一个输入框组件，并赋值给question_box变量，方便后续操作
-user_id_box = gr.inputs.Textbox(label="UserId",default=lambda:str(time.time())) # 创建一个输入框组件，并赋值给question_box变量，方便后续操作
-answer_boxes = []
-for i in range(0,10):
-    answer_boxes.append(gr.outputs.Textbox(label="Answer"+str(i))) # 创建一个输出框组件，并赋值给answer_box变量，方便后续操作
 
-interface = gr.Interface(
-    fn=ask_openai,
-    inputs=[question_box,user_id_box], 
-    outputs=answer_boxes,
-    share=True, # 启用分享功能，并生成公开链接 
-    analytics_enabled=True # 启用分析功能，并记录每个用户的输入和输出数据 
-)
+with gr.Blocks() as interface:
+    gr.Markdown(
+"""
+# Hello World!
+Start typing below to see the output.
+```python
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+```
+""")
+    with gr.Row():
+        user_id_box = gr.Textbox(label="UserId",value=lambda:str(time.time())) # 创建一个输入框组件，并赋值给question_box变量，方便后续操作
+        new_session_btn = gr.Button(value="NewSession")
 
-with interface:
-    user_id_box.set_event_trigger("load", seed, None, None)
+    with gr.Row():
+        # 创建一个UI界面，并指定输入和输出的类型
+        question_box = gr.Textbox(label="Question") # 创建一个输入框组件，并赋值给question_box变量，方便后续操作
+        submit_btn = gr.Button(value="Submit")
+    gr.Markdown(
+        """
+        # Output
+        """)
+    
+    answer_boxes = []
+    for i in range(0,10):
+        with gr.Row():
+            answer_boxes.append(gr.Markdown(label="Answer"+str(i))) # 创建一个输出框组件，并赋值给answer_box变量，方便后续操作
+    
+    def new_session():
+        ret=str(time.time())
+        #, list(map(lambda a:"",range(0,len(answer_boxes))))
+        return ret
+                
+    new_session_btn.click(fn=new_session,outputs=[user_id_box])
+    
+    submit_btn.click(fn=ask_openai,inputs=[question_box,user_id_box],outputs=answer_boxes)
+    question_box.submit(fn=ask_openai,inputs=[question_box,user_id_box],outputs=answer_boxes)
+    
 
 
 # 启动界面，并在浏览器中查看效果
